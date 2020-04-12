0b6587e3e60ead4aab1cfe002cee46a8a221c202
FilipNedovic <nedovic.filip@gmail.com>
2020-04-12 17:46:07 +0200

    added basic classes

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php
+<?php 
 
+    class Bolnica {}
 
+    class Odeljenje {}
+    
+    abstract class Osoba {}
 
+    class Doktor extends Osoba {}
+
+    class Pacijent extends Osoba {}
+
+    abstract class Pregled {}
+
+    class PregledKrvi extends Pregled {}
+
+    class PregledPluca extends Pregled {}
+
+    class PregledSrca extends Pregled {}
+
+    class Nalaz {}
+
+    class Recept {}
+
+    class Loger {}
 ?>
